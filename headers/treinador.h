c87f37f1716f27c944de260feead62b766377f4e
Gustavo Spínola <gus.spinola.com.br@gmail.com>
2026-09-19 13:53:29 -0300

    commit Tad treinador

---
+++ headers/treinador.h
@@ -0,0 +1,12 @@
+#ifndef TREINADOR_H
+#define TREINADOR_H
+
+typedef struct {
+    int id;
+    char nome[50];
+    int localizacao;
+    TIPO_pokelista pokelista;
+    int pokebolas;
+}   
+
+#endif
