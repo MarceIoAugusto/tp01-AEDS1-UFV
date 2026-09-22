348fb35150b193e4112d6b2b73a3bc3c03085630
Gustavo Spínola <gus.spinola.com.br@gmail.com>
2026-09-22 14:43:21 -0300

    anotaçoes do cabeçalho

--- headers/cabecalho.h
+++ headers/cabecalho.h
@@ -1,8 +1,10 @@
 #ifndef CABECALHO_H
 #define CABECALHO_H
-#define X 0
+#define X 0 // Exemplo: Treinador.pos[X], Treinador.pos[Y]
 #define Y 1
-#define TAM_NOME 50
-#define TAM_TIPO 50
+#define TAM_NOME 50 // Todos nomes vao ter o mesmo tamanho, ao inves de escrever "char nomepokemon[50]"" escrevemos
+                    //  "char nomepokemon/treinador[TAM_NOME]"" evita de fizermos nomes de tamanhos diferentes
+
+#define TAM_TIPO 50 // Mesma coisa do "TAM_NOME" porem esse e para o tipo do pokemon ex: "char tipo[TAM_TIPO]
 #define MAX_TREINADORES 2
 #endif
